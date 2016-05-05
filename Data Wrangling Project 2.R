289e5458d1c280292a8f7ee3f52a9e5b1fd737bf
rogercbryan <enfusen@gmail.com>
2016-05-05 17:59:45 -0400

    Final for Project 2

--- Data Wrangling Project 2.R
+++ Data Wrangling Project 2.R
@@ -36,4 +36,35 @@
 titanic.data <- read.csv("titanic_original.csv", stringsAsFactors = FALSE)
 str(titanic.data)
 titanic.data$embarked
+# The embarked column has one missing value, which is known to correspond to a passenger who actually embarked at Southampton. 
+# Find the missing value and replace it with S.
 titanic.data$embarked[[285]]
+
+# which embarked are missing variables
+missing_embarked <- which(titanic.data$embarked == "")
+titanic.data$embarked[missing_embarked] <- "S"
+
+# find the mean of age
+# issue is that NA is causing the mean to be NA
+mean(titanic.data$age, na.rm = TRUE)
+
+# summary is a generic function not the exact value
+summary(titanic.data$age)
+is.na(titanic.data$age)
+
+# How do you calculate the mean without looking at the NA
+# http://stackoverflow.com/questions/6148374/r-mean-when-conditions-got-na
+mean(titanic.data$age, na.rm = TRUE)
+# I chose this version because it worked
+
+# assign NA to missing boat numbers
+missing_boat <- which(titanic.data$boat == "")
+titanic.data$boat[missing_boat] <- NA
+
+# create a new column has_cabin_number which has 1 if there is a cabin number, and 0 otherwise
+# special value "NA" - is just letters |  vs string value NA - True NA (Not Available)
+# is.na(NA) = TRUE | is.na("NA") = FALSE
+
+titanic.data$has_cabin_number <- ifelse(titanic.data$cabin == "", 0, 1) 
+View(titanic.data)
+write.csv(titanic.data, "titanic_clean.csv")
